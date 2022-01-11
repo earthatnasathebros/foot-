5ee7aa2d9025b361d9af0558f6fdd6dbbd2597b8
chris russell <83202436+earthatnasathebros@users.noreply.github.com>
2022-01-11 17:06:20 +0000

    Create mechine.m

---
+++ mechine.m
@@ -0,0 +1,14 @@
+expr1 && expr2
+command = 'motor';
+[status,cmdout] = dos(command)
+	vec4=p.material('mat4"mvp  p*v*m)
+	addaxies ( (:p),randi 
+bandplot e=p.Eigen energy
+	
+	plot ((vec:4) ,randi (mat4 ^ p, v, m')
+	addaxies ( (:p),randi (mat4 ^ mvp  p*v*m), 'v)
+	addaxies ( (:v),randi (mat4 ^ mvp  p*v*m), 'e)
+	addaxies ( (:m),randi (mat4 ^ mvp  p*v*m), 'c)
+legend ({'plot1','plot2','plot3','plot4'})
+status =1
+If we assume the leg below the knee joint as one segment (no active joints), the kinetics is defined by three second-order equations, however, with the flexible geometry that introduces the ankle rotation:
