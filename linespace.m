b92b3bf2b2f52111bd78ea1d38e53b4a5a41d5d5
chris russell <83202436+earthatnasathebros@users.noreply.github.com>
2022-01-11 17:08:25 +0000

    Create linespace.m

---
+++ linespace.m
@@ -0,0 +1,9 @@
+figure 
+plot(curvefit,)
+(v = [mat4 -1][n,n]=size('e) 
+x=c *x
+mat4 ^ p, v, 'm') "30.25.75" axies
+v= linspace (30.25.75);
+    x= ((x=c *x)/(1+e.^mat4);
+    y= ((mat4  30.25.75') )/(1+c.^3));
+    plot(x,y)
